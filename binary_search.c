366955047981804e9931d2c67d5ba5c0dc38e1c9
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-06-19 13:18:23 +0530

    Update binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -1,3 +1,26 @@
+// Given a sorted array of  N integers representing the rollno of students in class and a target student  rollno, determine if the target student  exists in the class using the binary search algorithm. If student exists in the class, print the seat number of the student. Consider the seat number as the index of the sorted array.
+
+// Input Format
+
+// The first line reads the array of student register numbers
+
+// The second line reads the search element
+
+// Output Format
+
+// Seat number of the student
+
+
+
+
+
+// For example:
+
+// Input	Result
+// 100 101 102 103 104 105 105 106 107 108
+// 103
+// 3
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
